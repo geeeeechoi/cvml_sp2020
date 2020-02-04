e076a914c45fb70948a743527726b8e7099a0230
geeeeechoi <geeeeechoi@gmail.com>
2020-02-04 01:29:55 -0600

    assignment01

--- docs/code/in_class/day01/01_RozinMirror/scripts/main.js
+++ docs/code/in_class/day01/01_RozinMirror/scripts/main.js
@@ -1,5 +1,6 @@
 let myCapture;
 
+
 function setup() {
   let canvas = createCanvas(320,240,);
   myCapture = createCapture(VIDEO);
@@ -11,23 +12,32 @@ function setup() {
 
 function draw() {
   
-  background(30,50,150);
+  background(0);
   
   
-  //fill(random(255),random(255), random(255));
+  // fill(random(255),random(255), random(255));
   
   //load pixel data to myCapture object
   
   myCapture.loadPixels();
-  const stepSize = round(constrain(mouseX / 8,6,32));
+  
+   
+ const stepSize = round(constrain(mouseY / 4,10,32));
   
   for (let y = 0; y < height; y+=stepSize){
     for (let x = 0; x < width; x+=stepSize){
       const i = y * width + x;
-      const darkness = (255-myCapture.pixels[i * 4]) / 255;
-      const radius = stepSize * darkness; 
-    
+      const darkness = (0-myCapture.pixels[i * 4]) / 255;
+      const radius = stepSize *darkness;
       
+  if (mouseIsPressed){
+    fill(255);
+  } else {
+    fill (random(255), random(255),random(255));
+      }
+      
+  
+     
       push();
         translate(width, 0);
         scale(-1,1);
@@ -39,5 +49,7 @@ function draw() {
    
       
     }
+  
+ 
   
 }
