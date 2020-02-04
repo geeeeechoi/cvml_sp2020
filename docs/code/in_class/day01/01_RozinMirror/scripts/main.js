96869b45cb16e481650849d7f0c04cc5d6658cef
geeeeechoi <60239489+geeeeechoi@users.noreply.github.com>
2020-02-04 01:36:08 -0600

    Update main.js

--- docs/code/in_class/day01/01_RozinMirror/scripts/main.js
+++ docs/code/in_class/day01/01_RozinMirror/scripts/main.js
@@ -11,21 +11,29 @@ function setup() {
 
 function draw() {
   
-  background(30,50,150);
+  background(0);
   
   
-  //fill(random(255),random(255), random(255));
+  // fill(random(255),random(255), random(255));
   
   //load pixel data to myCapture object
   
   myCapture.loadPixels();
-  const stepSize = round(constrain(mouseX / 8,6,32));
+  
+   
+ const stepSize = round(constrain(mouseY / 4,10,32));
   
   for (let y = 0; y < height; y+=stepSize){
     for (let x = 0; x < width; x+=stepSize){
       const i = y * width + x;
-      const darkness = (255-myCapture.pixels[i * 4]) / 255;
-      const radius = stepSize * darkness; 
+      const darkness = (0-myCapture.pixels[i * 4]) / 255;
+      const radius = stepSize *darkness;
+      
+  if (mouseIsPressed){
+    fill(255);
+  } else {
+    fill (random(255), random(255),random(255));
+      }
     
       
       push();
